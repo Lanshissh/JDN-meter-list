e6388d1d64ea840a82132254ff682dbc1f030236
lanshissh <cabelance2@gmail.com>
2026-01-10 00:11:38 +0800

    fixing the import and export

--- constants/api.ts
+++ constants/api.ts
@@ -1,5 +1,5 @@
 //export const BASE_API = "http://192.168.200.132:3000";
 
-export const BASE_API = "http://192.168.100.215:3000"
+export const BASE_API = "http://192.168.100.172:3000"
 
 //export const BASE_API = "http://10.89.187.94:3000"
